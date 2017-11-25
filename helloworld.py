f8f4e68db8e271d8e19c5004447a27e461128074
wuwen1979 <wuwen1979@outlook.com>
2017-11-25 18:01:02 +0800

    print执行sql返回值

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,6 @@ conn=sqlite3.connect('//Users//wuwen//python//studypython//study-python//test.db
 cur=conn.cursor()
 sql="create table if not exists 't_user'('id' int,'name' char(24),'password' char(24))"
 str=cur.execute(sql)
-# print(str)
 print("create table successfully")
 conn.commit()
 cur.close()
