d84bb303d73bb301f9b62f56d59cf15e78843ed5
wuwen1979 <wuwen1979@outlook.com>
2017-11-25 16:01:25 +0800

    read write append file

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,27 @@
 #coding=utf-8
-print("哈哈")
+print("beginning to connect sqlite3")
 ###使用sqlite###
 import sqlite3
 conn=sqlite3.connect('//Users//wuwen//python//studypython//study-python//test.db')
 cur=conn.cursor()
 sql="create table if not exists 't_user'('id' int,'name' char(24),'password' char(24))"
 str=cur.execute(sql)
+# print(str)
 print("create table successfully")
 conn.commit()
 cur.close()
+
+#write file#
+file1=open("wuwen.txt","w")
+file1.write("你好 \n")
+file1.close()
+
+#append file#
+file2=open("wuwen.txt","a")
+file2.write("李小喵")
+file2.close()
+
+#read file#
+file=open("wuwen.txt","r")
+print(file.read())
+file.close()
