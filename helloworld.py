618f38dfbf78c7b15e8dc374d9fb55e763710205
wuwen1979 <wuwen1979@outlook.com>
2017-11-25 15:21:12 +0800

    sqlite3
    study how to python connect sqlite3

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
-print("你好，世界")
+#coding=utf-8
+print("哈哈")
+###使用sqlite###
+import sqlite3
+conn=sqlite3.connect('//Users//wuwen//python//studypython//study-python//test.db')
+cur=conn.cursor()
+sql="create table if not exists 't_user'('id' int,'name' char(24),'password' char(24))"
+str=cur.execute(sql)
+print("create table successfully")
+conn.commit()
+cur.close()
